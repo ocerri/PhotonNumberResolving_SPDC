1aef79df049e2957429bf6ea331588c0d431fcf9
Olmo Cerri <olmo.cerri@gmail.com>
2022-01-27 21:17:39 -0800

    New utils

---
+++ lib/stat_utils.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def correlation_from_covariance(covariance):
+    v = np.sqrt(np.diag(covariance))
+    outer_v = np.outer(v, v)
+    correlation = covariance / outer_v
+    correlation[covariance == 0] = 0
+    return correlation
